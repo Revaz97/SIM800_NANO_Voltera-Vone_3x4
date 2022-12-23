34bba735b5db19d34967fe355dac541b64183752
Revaz97 <75586133+Revaz97@users.noreply.github.com>
2022-12-23 12:02:02 +0400

    Update Sim800l_with_Arduino.ino

--- Arduino code/Sim800l_with_Arduino/Sim800l_with_Arduino.ino
+++ Arduino code/Sim800l_with_Arduino/Sim800l_with_Arduino.ino
@@ -3,13 +3,17 @@
 #include <SoftwareSerial.h>
 #include <avr/wdt.h>
 
-#define ONE_WIRE_BUS 13
+#define Relay1 6
+#define Relay2 7
+#define Mains_sence 5
+
+#define ONE_WIRE_BUS 12
+
 DallasTemperature sensors(&oneWire);
 
 
-SoftwareSerial sim(10, 11);
+SoftwareSerial sim(2, 3);
 String number = "+995577651913";
-
 
 int b = 0;
 int i;
@@ -19,62 +23,23 @@ void setup()
 { 
   Serial.begin(9600);
   wdt_enable(WDTO_2S);
-  pinMode(5, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode (Mains_sence, INPUT)
+  pinMode (Relay1, OUTPUT)
+  pinMode (Relay2, OUTPUT)  
+
   sim.begin();
   sensors.begin();
-
-  
-  }
-
+}
 
 void loop() 
 {
-
- Serial.print("Requesting temperatures...");
- sensors.requestTemperatures();
- Serial.println("DONE");
-  float tempC = sensors.getTempCByIndex(0);
-  if (tempC != DEVICE_DISCONNECTED_C)
+  if (Mains_sence == 1)
   {
-    Serial.print("Temperature for the device 1 (index 0) is: ");
-    Serial.println(tempC);
+    SendSMS
   }
-  else
-  {
-    Serial.println("Error: Could not read temperature data");
-  }
-}
-
-    
- Serial.println(digitalRead(5));
-  
-
-    if (digitalRead(5) == HIGH)
-   {
-    digitalWrite(LED_BUILTIN, HIGH);
-    b = 0; 
-    }
-
-    
-/*if (digitalRead(5) == LOW && b < 5)
- {
-  digitalWrite(LED_BUILTIN, LOW);
-  wdt_disable();
-   for(i = 0; i<5; i++)
-    {      
-      Serial.print("b = ");
-      Serial.println(b);
-      b++;
-      Serial.print("Calling ");
-      Serial.println(number);      
-      callNumber();
-      delay(5000);
-    }
-     */
-  }
-    delay(100);
-    wdt_reset(); 
+  delay(10);
+  wdt_reset(); 
 }
 
 
@@ -90,4 +55,39 @@ void SendStatusSMS()
   sim.print ("AT+CMGF=1");
   sim.print ("AT+CMGS=\"+995577651913\"");
   sim.print (status);
+}
+
+
+
+void Relay1_restart()
+{
+  digitalWrite(Relay1, LOW)
+  delay (5000);
+  digitalWrite(Relay1, HIGH)
+}
+
+void Relay2_restart()
+{
+  digitalWrite(Relay2, LOW)
+  delay (5000);
+  digitalWrite(Relay2, HIGH)
+}
+
+
+
+void GetTemp()
+{
+  Serial.print("Requesting temperatures...");
+ sensors.requestTemperatures();
+ Serial.println("DONE");
+  float tempC = sensors.getTempCByIndex(0);
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+    Serial.print("Temperature for the device 1 is: ");
+    Serial.println(tempC);
+  }
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
 }
